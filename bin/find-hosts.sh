12b577b10a36625713355423afe9e8226bc9703f
qianghu <qianghu@deeproute.ai>
2021-04-14 12:13:16 +0800

    fix: add cache dir not exits auto create

--- bin/find-hosts.sh
+++ bin/find-hosts.sh
@@ -2,7 +2,9 @@
 
 CACHE_DIR="$HOME/.jumphost/cache"
 GEN_DIR="$HOME/.jumphost/generator.d"
-
+if [ ! -d $CACHE_DIR ]; then
+  mkdir -p $CACHE_DIR
+fi
 for i in $(find $GEN_DIR -name '*.sh' -type f); do [ -x $i ] && $i; done
 
 grep --no-filename -E "^$1" $CACHE_DIR/*.findhost.tsv
